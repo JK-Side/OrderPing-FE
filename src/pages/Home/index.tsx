35b1ef1b224409bc0b4a91d264699e00b3a003f9
JeongWon-CHO <ahin0312@naver.com>
2026-01-02 10:49:35 +0900

    feat: 비로그인 상태 홈 UI 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import AuthedHome from './components/AuthedHome';
 import GuestHome from './components/GuestHome';
 
 export default function Home() {
-  const [isLogin] = useState(true);
+  const [isLogin] = useState(false);
 
   return <>{isLogin ? <AuthedHome /> : <GuestHome />}</>;
 }
