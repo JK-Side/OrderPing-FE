256d9b01a952b1d9819f68731447e9a2d10bdb1f
JeongWon-CHO <ahin0312@naver.com>
2026-01-02 17:36:09 +0900

    feat: 로그인/로그아웃 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,9 +1,34 @@
 import { Link } from 'react-router-dom';
+import { postLogout } from '@/api/auth';
 import OrderPingLogo from '@/assets/logo/ORDERPING_LOGO_TEXT.png';
 import styles from './Header.module.scss';
 
+const BASE_URL = import.meta.env.VITE_KAKAO_LOGIN;
+const ACCESS_TOKEN_KEY = 'AUTH_TOKEN_KEY';
+
 export default function Header() {
-  const isLogin = true; // 후에 로그인 확인 훅(ex. useAuth())으로 교체
+  const isLoggedIn = !!localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  const handleKakaoLogin = () => {
+    window.location.href = `${BASE_URL}`;
+  };
+
+  const handleLogout = async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    try {
+      if (refreshToken) {
+        await postLogout(refreshToken);
+      }
+    } catch (e) {
+      console.error('logout error', e);
+    } finally {
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+      localStorage.removeItem('refreshToken');
+
+      window.location.href = '/';
+    }
+  };
 
   return (
     <header className={styles.header}>
@@ -12,23 +37,21 @@ export default function Header() {
       </Link>
 
       <nav className={styles.nav}>
-        {isLogin ? (
+        {isLoggedIn ? (
           <>
             <Link to="/mypage" className={styles.navItem}>
               마이페이지
             </Link>
-            <button type="button" className={styles.navItem}>
+            <button type="button" className={styles.navItem} onClick={handleLogout}>
               로그아웃
             </button>
           </>
         ) : (
-          <Link to="/login" className={styles.navItem}>
+          <button type="button" className={styles.navItem} onClick={handleKakaoLogin}>
             로그인
-          </Link>
+          </button>
         )}
       </nav>
     </header>
   );
 }
-
-/* 머하지.. 커밋하고 홈화면 UI 상하차 ㄱ */
