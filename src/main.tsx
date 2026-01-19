b4ca9dcbe16f2ea4797c272efc47cab93675f19d
JeongWon-CHO <ahin0312@naver.com>
2026-01-19 10:19:37 +0900

    feat: toast 컴포넌트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastProvider } from '@/components/Toast';
 import '@/styles/global.scss';
 import App from './App';
 
@@ -16,9 +17,11 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
