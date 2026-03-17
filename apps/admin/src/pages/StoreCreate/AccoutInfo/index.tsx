d833652d27a9b6d4043d66c334e9c6d718dc3bf5
JeongWon-CHO <ahin0312@naver.com>
2026-03-17 14:39:11 +0900

    feat: 예금주명, 계좌번호 글자수 제한 추가

--- apps/admin/src/pages/StoreCreate/AccoutInfo/index.tsx
+++ apps/admin/src/pages/StoreCreate/AccoutInfo/index.tsx
@@ -75,19 +75,30 @@ export default function AccoutInfo({
             messageState={errors.accountHolder ? 'error' : undefined}
           >
             <Input.Text
-              placeholder="예금주명을 입력해 주세요."
+              placeholder="예금주명을 입력해 주세요. (최대 6글자)"
               {...register('accountHolder', {
                 required: '예금주명을 입력해 주세요.',
+                maxLength: { value: 6, message: '예금주명은 최대 6자입니다.' },
               })}
             />
           </Input>
 
-          <Input label="계좌번호" required message="입력한 계좌 정보는 주점 정산 용도로만 사용돼요" messageState="info">
+          <Input
+            label="계좌번호"
+            required
+            message={errors.accountNumber?.message || '입력한 계좌 정보는 주점 정산 용도로만 사용돼요'}
+            messageState={errors.accountNumber ? 'error' : 'info'}
+          >
             <Input.Text
-              placeholder="계좌번호를 숫자만 입력해 주세요."
+              placeholder="계좌번호를 숫자만 입력해 주세요. (최대 20글자)"
               inputMode="numeric"
               {...register('accountNumber', {
                 required: '계좌번호를 입력해 주세요.',
+                maxLength: { value: 20, message: '계좌번호은 최대 20자입니다.' },
+                onChange: (e) => {
+                  const value = e.target.value;
+                  e.target.value = value.replace(/[^0-9]/g, '');
+                },
               })}
             />
           </Input>
